d97658b7a93d8f1276952add1114d70be63cf5c3
FutureGenAi <future.genaiml@gmail.com>
2024-07-14 14:10:47 +0530

    setup update

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "Text-Summarizer-Project"
-AUTHOR_USER_NAME = "FutureGenAI"
+AUTHOR_USER_NAME = "FutureGenAi"
 SRC_REPO = "textSummarizer"
-AUTHOR_EMAIL = "pratik.941@gmail.com"
+AUTHOR_EMAIL = "future.genaiml@gmail.com"
 
 
 
